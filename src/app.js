5c9a5d2a9a071e4dd4e96bf9fce243e59fb4a9fb
Ross Borchers <rossborchers@gmail.com>
2023-05-25 11:38:54 +0200

    Basic week update working

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ const togglApiToken = process.env.TOGGL_API_TOKEN;
 const togglApiUrl = 'https://api.track.toggl.com/api/v8/time_entries';
 const togglProjectId = process.env.TOGGL_PROJECT_ID;
 
+//Avoiding toggl API rate limiting
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
+
+
 const createTogglTimeEntry = async (description, pid, startDateTime, duration) => {
     const data = {
         time_entry: {
@@ -25,38 +31,75 @@ const createTogglTimeEntry = async (description, pid, startDateTime, duration) =
 
     try {
         const response = await axios.post(togglApiUrl, data, config);
-        console.log('Time entry created successfully:', response.data);
+        console.log('\nTime entry created successfully:', response.data);
     } catch (error) {
-        console.log('Error creating time entry:', error);
+        if(error.response && error.response.status === 429) {
+              //We have hit the rate limit... try again after waiting a few seconds
+              console.log('\n=====! Hit rate limit. Waiting and trying again in 5 seconds !=====');
+              await sleep(5000);
+              await createTogglTimeEntry(description, pid, startDateTime, duration);
+        }
+        else{
+            console.log('\nERROR creating time entry:', error);
+        }
+       
     }
 };
 
 
 const createTimeBlock = async (description, projectId, startDate, hours) => {
 
-    createTogglTimeEntry(description, projectId, startDate, hours * 3600);
+    await createTogglTimeEntry(description, projectId, startDate, hours * 3600);
 };
 
 
+//To use directly: createDefaultWorkday("My description", togglProjectId, '05/25/2023');
 const createDefaultWorkday = async (description, projectId, dayStr) => {
 
     //Creates a default workday. 3 hours. 9-12. Break 12-1. 4 hours. 1-5.
     const startDateTime = new Date(dayStr);
 
-    startDateTime.setHours(9, 0);
-    createTimeBlock(description, projectId, startDateTime, 3);
     
-    startDateTime.setHours(13, 0);
-    createTimeBlock(description, projectId, startDateTime, 4);
-};
+    startDateTime.setHours(9, 0);
+    
+    await createTimeBlock(description, projectId, startDateTime, 3);
+    
+    //pre-emptivley avoid rate limit
+    await sleep(1000);
 
-//createDefaultWorkday("Test", togglProjectId, '05/25/2023');
+    startDateTime.setHours(13, 0);
+    await createTimeBlock(description, projectId, startDateTime, 4);
+
+     //pre-emptivley avoid rate limit
+    await sleep(1000); 
+};
 
 const createDefaultWorkWeek = async (description, projectId, anyDayInWeek) => {
+    // Create a new date object from the ISO string
+    const date = new Date(anyDayInWeek);
 
-    createDefaultWorkday(description, projectId, '05/25/2023');
-    createDefaultWorkday(description, projectId, '05/25/2023');
-    createDefaultWorkday(description, projectId, '05/25/2023');
-    createDefaultWorkday(description, projectId, '05/25/2023');
-    createDefaultWorkday(description, projectId, '05/25/2023');
+    // Find the previous Monday or the current day if it is Monday
+    while (date.getDay() !== 1) {
+        date.setDate(date.getDate() - 1);
+    }
+
+    // Array of 5 workdays in a week, from Monday to Friday
+    const workdays = [0, 1, 2, 3, 4];
+
+    // For each workday, add the day to the date and call `createDefaultWorkday`
+    for (const day of workdays) {
+        const workday = new Date(date);
+        workday.setDate(workday.getDate() + day);
+
+        // Format the workday to a string in mm/dd/yyyy format
+        const dayStr = `${workday.getMonth() + 1}/${workday.getDate()}/${workday.getFullYear()}`;
+        
+        await createDefaultWorkday(description, projectId, dayStr);
+    }
 };
+
+createDefaultWorkWeek("Boss", togglProjectId, '05/15/2023');
+createDefaultWorkWeek("Localisation", togglProjectId, '05/9/2023');
+createDefaultWorkWeek("Playtesting and quick wins", togglProjectId, '05/2/2023');
+createDefaultWorkWeek("Shadow hiding and style volumes", togglProjectId, '04/25/2023');
+createDefaultWorkWeek("Fixing broken runtime gizmos + Finishing offset maps", togglProjectId, '04/18/2023');
